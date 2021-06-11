ba9e513dfea693d1d35a46279e9dec167b08c8a7
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-06-11 20:40:11 +0300

    Add some features

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,42 +1,44 @@
 <script>
-	let recipientName = "";
-	let settingState = 0;
-	let peoplemass = [];
-	function funcChoiceChat(name) {
-		recipientName = name;
-	}
-	function openSettings() {
-		settingState = !settingState;
-	}
+  let recipientName = "";
+  let settingState = 0;
+  let peoplemass = [];
+
+  function funcChoiceChat(name) {
+    recipientName = name;
+  }
+
+  function openSettings() {
+    settingState = !settingState;
+  }
 </script>
 
 <div>
-	<div>
-		<input class="settingInput" placeholder="Search">
-	</div>
-	<div>
-		{#if (!settingState)}
-			<div>
-				<div>
-					{#each peoplemass as man}
-						<div on:click={() => funcChoiceChat(man.name)}>
-							<h4>Account name</h4>
-						</div>
-					{/each}
-				</div>
-			</div>
-		{:else }
-			<div>
-				<div>
-					<h3>Settings</h3>
-				</div>
-			</div>
-		{/if}
+  <div>
+    <input class="settingInput" placeholder="Search">
+  </div>
+  <div>
+    {#if (!settingState)}
+      <div>
+        <div>
+          {#each peoplemass as man}
+            <div on:click={() => funcChoiceChat(man.name)}>
+              <h4>Account name</h4>
+            </div>
+          {/each}
+        </div>
+      </div>
+    {:else }
+      <div>
+        <div>
+          <h3>Settings</h3>
+        </div>
+      </div>
+    {/if}
 
-		{#if recipientName === ""}
-			<h4>Please select a chat to start messaging</h4>
-		{:else}
-			//тут будет чат
-		{/if}
-	</div>
+    {#if recipientName === ""}
+      <h4>Please select a chat to start messaging</h4>
+    {:else}
+      //тут будет чат
+    {/if}
+  </div>
 </div>
