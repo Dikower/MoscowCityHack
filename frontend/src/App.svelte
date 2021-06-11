d045684ce6aafb553993134dc37b3875fa3e91c3
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-06-11 20:38:30 +0300

    First step

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,42 @@
 <script>
-	export let name;
+	let recipientName = "";
+	let settingState = 0;
+	let peoplemass = [];
+	function funcChoiceChat(name) {
+		recipientName = name;
+	}
+	function openSettings() {
+		settingState = !settingState;
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div>
+	<div>
+		<input class="settingInput" placeholder="Search">
+	</div>
+	<div>
+		{#if (!settingState)}
+			<div>
+				<div>
+					{#each peoplemass as man}
+						<div on:click={() => funcChoiceChat(man.name)}>
+							<h4>Account name</h4>
+						</div>
+					{/each}
+				</div>
+			</div>
+		{:else }
+			<div>
+				<div>
+					<h3>Settings</h3>
+				</div>
+			</div>
+		{/if}
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+		{#if recipientName === ""}
+			<h4>Please select a chat to start messaging</h4>
+		{:else}
+			//тут будет чат
+		{/if}
+	</div>
+</div>
