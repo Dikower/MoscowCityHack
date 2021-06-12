b82a1b83af84b4f908b9f0a0319850e19840ac3f
Daria Petrova <dar99p@yandex.ru>
2021-06-12 12:38:13 +0300

    Update audio_model.py

--- ai/audio_model/audio_model.py
+++ ai/audio_model/audio_model.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 
 class AudioModel:
@@ -11,6 +12,4 @@ class AudioModel:
 
         with open('res.json') as f:
             d = json.load(f)
-            print(d['text'])
-            print(key_words.find_key_words(d['text']))
             return json.dumps({'success': True, 'content': d['text']}), 200, {'ContentType': 'application/json'}
