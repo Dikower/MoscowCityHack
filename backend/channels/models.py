fedf37e107af3b402f566c9715d25784b2072398
Dmitriy Din <dmitriy1d01@gmail.com>
2021-06-12 18:01:47 +0300

    Changed backend structure

--- backend/channels/models.py
+++
@@ -1,7 +0,0 @@
-from tortoise.models import Model
-from tortoise import fields
-
-
-class Channel(Model):
-    id = fields.IntField(pk=True)
-    subs = fields.ManyToManyField('users.User', related_name='channels')
--- backend/chats/models.py
+++
@@ -1,6 +0,0 @@
-from tortoise import Model, Tortoise, fields
-
-
-class Chat(Model):
-    id = fields.IntField(pk=True)
-
