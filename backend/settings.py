8c066f68999e49027d36b38cd9bc1d4a8acbcff1
Dmitriy Din <dmitriy1d01@gmail.com>
2021-06-12 10:26:09 +0300

    Added templates

---
+++ backend/settings.py
@@ -0,0 +1,38 @@
+from glob import glob
+import json
+
+with open('.gitignore', 'r', encoding='utf8') as file:
+    exclude = set(file.read().split('\n'))
+
+folders = list(set([folder.strip('\\') for folder in glob("*/")]) - exclude)
+# apps = {
+#     'bots': {
+#         'models': ['backend.bots.models'],
+#     },
+#     'chats': {
+#         'models': ['backend.chats.models'],
+#     },
+#     'users': {
+#         'models': ['backend.users.models'],
+#     },
+#     'channels': {
+#         'models': ['backend.channels.models'],
+#     }
+# }
+
+apps = {folder: {'models': [f'backend.{folder}.models']} for folder in folders}
+PROD_TORTOISE_ORM = {
+    'connections': {'default': 'sqlite://db/prod/db.sqlite3'},
+    'apps': apps
+}
+
+TEST_TORTOISE_ORM = {
+    'connections': {'default': 'sqlite://db/test/db.sqlite3'},
+    'apps': apps
+}
+
+
+with open('secrets.json') as file:
+    SECRET_KEY = json.load(file)
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
