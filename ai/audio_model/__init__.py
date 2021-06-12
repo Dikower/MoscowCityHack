339d810611f4f5c4413482660938c20adb93ead9
Daria Petrova <dar99p@yandex.ru>
2021-06-12 15:56:25 +0300

    audio_model_upd

--- ai/audio_model/__init__.py
+++ ai/audio_model/__init__.py
@@ -1,5 +1,15 @@
+from vosk import Model, KaldiRecognizer, SetLogLevel
+import sys
 import os
+import wave
+import subprocess
+
 import json
+import string
+
+import os
+import sys
+
 
 """
 Конвертирует аудиофайл в текст, возвращает текст.
@@ -9,12 +19,25 @@ import json
 
 class AudioModel:
     def __init__(self):
-        self.command = 'python run_model.py '
+        SetLogLevel(0)
+        self.sample_rate = 16000
+        self.model = Model("model")
+        self.rec = KaldiRecognizer(self.model, self.sample_rate)
 
     def upload_file(self, path):
-        os.popen(self.command + path)
+        process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i', path,
+                                    '-ar', str(self.sample_rate), '-ac', '1', '-f', 's16le', '-'],
+                                   stdout=subprocess.PIPE)
 
-        with open('res.json', 'r', encoding='utf8') as f:
-            d = json.load(f)
-            return d['text']
-            # return json.dumps({'success': True, 'content': d['text']}), 200, {'ContentType': 'application/json'}
+        while True:
+            data = process.stdout.read(4000)
+            if len(data) == 0:
+                break
+            if self.rec.AcceptWaveform(data):
+                pass
+            else:
+                pass
+
+        finish = str(self.rec.FinalResult())
+
+        return finish
