8db99d2540480152410969f19f95522566196245
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-06-13 12:01:37 +0300

    auth logic

---
+++ frontend/src/authWindow.svelte
@@ -0,0 +1,11 @@
+<script>
+  import {ID} from "./storage";
+  function up(){
+    localStorage.setItem("ID", "2e23:de42:de52:aw41");
+    ID.set("2e23:de42:de52:aw41");
+  }
+</script>
+
+
+<h1>Auth</h1>
+<button on:click={up}></button>
