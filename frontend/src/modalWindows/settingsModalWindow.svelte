584ca2c75f89e439be4a85e4db99b7bc337cde9d
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-06-14 03:27:12 +0300

    графики

--- frontend/src/modalWindows/settingsModalWindow.svelte
+++ frontend/src/modalWindows/settingsModalWindow.svelte
@@ -1,10 +1,76 @@
 <script>
   import {settingWindowState} from '../storage.js';
+  import Line from "svelte-chartjs/src/Line.svelte"
+
 
   let checkMat = false;
   let checkToxic = false;
   let checkPrInfo = false;
   let changeState = false;
+  let secondWindowState = false;
+  export let status = "good";
+
+  let x = 5;
+  let y = 5;
+  let z = 5;
+
+  const colors = {
+    good: {backgroundColor: 'rgba(43, 192, 22, 0.2)', borderColor: 'rgb(43, 192, 22)'},
+    normal: {backgroundColor: 'rgb(245, 171, 0, 0.2)', borderColor: 'rgb(245, 171, 0)'},
+    bad: {backgroundColor: 'rgb(234, 43, 31, 0.2)', borderColor: 'rgb(234, 43, 31)'}
+  }
+  let fT = x * y * z;
+
+  function refT() {
+    fT = x * y * z  / 12;
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  $: data = [fT, fT*1.2*z, fT*1.4*x, fT*1.3*y, fT*1.5*x, fT*1.9*x, fT*1.8*y, fT*1.7*4];
+  let drawData;
+  $: {
+    data = data.slice(0, 20);
+    drawData = {
+      labels: ['5s', '10s', '15s', '20s', '25s', '30s', '35s', '40s', '45s', '50s', '55s', '60s'],
+      datasets: [{
+        data: data,
+        tension: 0.0,
+        borderColor: colors[status].borderColor,
+        backgroundColor: colors[status].backgroundColor,
+        pointRadius: 3,
+        borderWidth: 2
+      }]
+    };
+  }
+  const options = {
+    animation: false,
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        ticks: {
+          fontSize: 10,
+          fontColor: 'lightgrey'
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          fontSize: 10,
+          fontColor: 'lightgrey',
+          maxTicksLimit: 10,
+          padding: 25,
+        }
+      }]
+    },
+    tooltips: {
+      backgroundColor: '#1e90ff'
+    }
+  }
 
   let userData = {
     name: "Димочка",
@@ -28,6 +94,11 @@
     userData.name = name;
     changeState = !changeState;
   }
+
+  function nextWindow() {
+    secondWindowState = !secondWindowState;
+  }
+
 </script>
 
 <div id="TB_overlay">
@@ -35,50 +106,73 @@
 
 <div class="wrapper">
   <div class="WindowBox">
-    <div class="BoxForSetting">
-      <button on:click={closeWindow} class="cancel-button">
-        <img src="cancel.svg" class="cancel-icon" alt="cancel-icon"/>
-      </button>
-      <div class="userInfo">
-        <img src={userData.img}>
-        {#if !changeState}
-          <div class="user-name">
-            <h1>{userData.name}</h1>
-            <button on:click={changeName} class="edit-button">
-              <img src="edit.svg" class="edit-icon" alt="edit-icon"/>
-            </button>
-          </div>
-        {:else}
-          <input bind:value={name}>
-          <button on:click={saveName} class="saveButton">Сохранить</button>
-        {/if}
+    {#if !secondWindowState}
+      <div class="BoxForSetting">
+        <button on:click={closeWindow} class="cancel-button">
+          <img src="cancel.svg" class="cancel-icon" alt="cancel-icon"/>
+        </button>
+        <div class="userInfo">
+          <img src={userData.img}>
+          {#if !changeState}
+            <div class="user-name">
+              <h1>{userData.name}</h1>
+              <button on:click={changeName} class="edit-button">
+                <img src="edit.svg" class="edit-icon" alt="edit-icon"/>
+              </button>
+            </div>
+          {:else}
+            <input bind:value={name}>
+            <button on:click={saveName} class="saveButton">Сохранить</button>
+          {/if}
 
-      </div>
+        </div>
 
-      <div class="oneSettingBox">
-        <h3>Скрытие матов</h3>
-        <label class="switch">
-          <input type="checkbox" checked={checkMat}>
-          <span class="slider round"></span>
-        </label>
+        <div class="oneSettingBox">
+          <h3>Скрытие матов</h3>
+          <label class="switch">
+            <input type="checkbox" checked={checkMat}>
+            <span class="slider round"></span>
+          </label>
+        </div>
+        <div class="oneSettingBox">
+          <h3>Проверка на токсичность</h3>
+          <label class="switch">
+            <input type="checkbox" checked={checkToxic}>
+            <span class="slider round"></span>
+          </label>
+        </div>
+        <div class="oneSettingBox">
+          <h3>Проверка на приватную информацию</h3>
+          <label class="switch">
+            <input type="checkbox" checked={checkPrInfo}>
+            <span class="slider round"></span>
+          </label>
+        </div>
+        <div class="buttonsBox">
+          <button on:click={nextWindow} class="main-button">Доп. настройки</button>
+          <button on:click={saveSettings} class="main-button">Сохранить</button>
+        </div>
       </div>
-      <div class="oneSettingBox">
-        <h3>Проверка на токсичность</h3>
-        <label class="switch">
-          <input type="checkbox" checked={checkToxic}>
-          <span class="slider round"></span>
-        </label>
-      </div>
-      <div class="oneSettingBox">
-        <h3>Проверка на приватную информацию</h3>
-        <label class="switch">
-          <input type="checkbox" checked={checkPrInfo}>
-          <span class="slider round"></span>
-        </label>
-      </div>
-      <button on:click={saveSettings} class="main-button">Сохранить</button>
-    </div>
+    {:else}
+      <div class="BoxForSetting">
+        <div class="oneSetForG">
+          <h1>Первый ползунок для x - </h1>
+          <input type="number" bind:value={x} on:input={refT}>
+        </div>
 
+        <div class="oneSetForG">
+          <h1>Первый ползунок для y - </h1>
+          <input type="number" bind:value={y} on:input={refT}>
+        </div>
+
+        <div class="oneSetForG">
+          <h1>Первый ползунок для z - </h1>
+          <input type="number" bind:value={z} on:input={refT}>
+        </div>
+        <Line data={drawData} {options}/>
+        <button on:click={nextWindow} class="main-button">Сохранить</button>
+      </div>
+    {/if}
   </div>
 </div>
 
@@ -231,6 +325,24 @@
     outline: none;
     margin-top: 15px;
     margin-bottom: 15px;
-    margin-left: auto;
+    /*margin-left: auto;*/
+  }
+
+  .buttonsBox {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+  }
+
+  .oneSetForG {
+    display: flex;
+  }
+
+  .oneSetForG input {
+    width: 50px;
+    height: 30px;
+    margin-left: 10px;
+    margin-top: auto;
+    margin-bottom: auto;
   }
 </style>
